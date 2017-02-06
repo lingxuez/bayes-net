7f58ec29b05459447ab61dea42af0eb9417a44ee
Lingxue Zhu <lingxue.zhu@gmail.com>
2017-02-05 23:56:04 -0500

    re-organized classes to BN.py; finished iequiv.py

---
+++ src/BN.py
@@ -0,0 +1,219 @@
+####################################################
+## Bayesian Networks
+## Lingxue Zhu
+## 2017/02/05
+####################################################
+
+import copy
+
+class Node(object):
+    """
+    Node in a directed graph
+    """
+    def __init__(self, name=""):
+        """
+        Construct a new node, and initialize the list of parents and children.
+        Each parent/child is represented by a (key, value) pair in dictionary, 
+        where key is the parent/child's name, and value is an Node object.
+
+        Args:
+            name: a unique string identifier.
+        """
+        self.name = name
+        self.parents = dict()
+        self.children = dict()
+
+    def add_parent(self, parent):
+        """
+        Args:
+            parent: an Node object.
+        """
+        if not isinstance(parent, Node):
+            raise ValueError("Parent must be an instance of Node class.")
+        pname = parent.name
+        self.parents[pname] = parent
+
+    def add_child(self, child):
+        """
+        Args:
+            child: an Node object.
+        """
+        if not isinstance(child, Node):
+            raise ValueError("Parent must be an instance of Node class.")
+        cname = child.name
+        self.children[cname] = child
+
+
+class BN(object):
+    """
+    Bayesian Network
+    """
+    def __init__(self):
+        """
+        Initialize the list of nodes in the graph.
+        Each node is represented by a (key, value) pair in dictionary, 
+        where key is the node's name, and value is an Node object
+        """
+        self.nodes = dict()
+
+    def add_edge(self, edge):
+        """
+        Add a directed edge to the graph.
+        
+        Args:
+            edge: a tuple (A, B) representing a directed edge A-->B,
+                where A, B are two strings representing the nodes' names
+        """
+        (pname, cname) = edge
+
+        ## construct a new node if it doesn't exist
+        if pname not in self.nodes:
+            self.nodes[pname] = Node(name=pname)
+        if cname not in self.nodes:
+            self.nodes[cname] = Node(name=cname)
+
+        ## add edge
+        parent = self.nodes.get(pname)
+        child = self.nodes.get(cname) 
+        parent.add_child(child)
+        child.add_parent(parent)
+
+    def print_graph(self):
+        """
+        Visualize the current graph.
+        """
+        print "Bayes Network:"
+        for nname, node in self.nodes.iteritems():
+            print "\tNode " + nname
+            print "\t\tParents: " + str(node.parents.keys())
+            print "\t\tChildren: " + str(node.children.keys())
+
+    def find_obs_anc(self, observed):
+        """
+        Traverse the graph, find all nodes that have observed descendants.
+
+        Args:
+            observed: a list of strings, names of the observed nodes. 
+
+        Returns:
+            a list of strings for the nodes' names for all nodes 
+            with observed descendants.
+        """
+        visit_nodes = copy.copy(observed) ## nodes to visit
+        obs_anc = set() ## observed nodes and their ancestors
+
+        ## repeatedly visit the nodes' parents
+        while len(visit_nodes) > 0:
+            next_node = self.nodes[visit_nodes.pop()]
+            ## add its' parents
+            for parent in next_node.parents:
+                obs_anc.add(parent)
+
+        return obs_anc
+
+    def is_dsep(self, start, end, observed):
+        """
+        Check whether start and end are d-separated given observed.
+        This algorithm mainly follows the "Reachable" procedure in 
+        Koller and Friedman (2009), 
+        "Probabilistic Graphical Models: Principles and Techniques", page 75.
+
+        Args:
+            start: a string, name of the first query node
+            end: a string, name of the second query node
+            observed: a list of strings, names of the observed nodes. 
+        """
+
+        ## all nodes having observed descendants.
+        obs_anc = self.find_obs_anc(observed)
+
+        ## Try all active paths starting from the node "start".
+        ## If any of the paths reaches the node "end", 
+        ## then "start" and "end" are *not* d-separated.
+        ## In order to deal with v-structures, 
+        ## we need to keep track of the direction of traversal:
+        ## "up" if traveled from child to parent, and "down" otherwise.
+        via_nodes = [(start, "up")]
+        visited = set() ## keep track of visited nodes to avoid cyclic paths
+
+        while len(via_nodes) > 0: 
+            (nname, direction) = via_nodes.pop()
+            node = self.nodes[nname]
+
+            ## skip visited nodes
+            if (nname, direction) not in visited:
+                visited.add((nname, direction)) 
+
+                ## if reaches the node "end", then it is not d-separated
+                if nname not in observed and nname == end:
+                    return False
+
+                ## if traversing from children, then it won't be a v-structure
+                ## the path is active as long as the current node is unobserved
+                if direction == "up" and nname not in observed:
+                    for parent in node.parents:
+                        via_nodes.append((parent, "up"))
+                    for child in node.children:
+                        via_nodes.append((child, "down"))
+                ## if traversing from parents, then need to check v-structure
+                elif direction == "down":
+                    ## path to children is always active
+                    if nname not in observed: 
+                        for child in node.children:
+                            via_nodes.append((child, "down"))
+                    ## path to parent forms a v-structure
+                    if nname in observed or nname in obs_anc: 
+                        for parent in node.parents:
+                            via_nodes.append((parent, "up"))
+        return True
+
+    def get_skeleton(self):
+        """
+        Find the skeleton of the Bayesian Network.
+        Returns: 
+            a set of undirected edges (represented by tuples) in the skeleton. 
+        """
+        skeleton = set()
+        for (nname, node) in self.nodes.iteritems():
+            ## add edges to skeleton
+            for parent in node.parents:
+                if (nname, parent) not in skeleton and (parent, nname) not in skeleton:
+                    skeleton.add((nname, parent))
+            for child in node.parents:
+                if (nname, child) not in skeleton and (child, nname) not in skeleton:
+                    skeleton.add((nname, child))
+
+        return skeleton
+
+    def get_skeleton_immor(self):
+        """
+        Find the skeleton and immoralities in the graph.
+        Returns:
+            a tuple, the first element is a set of edges in the skeleton,
+            and the second element is a set of parent-pairs that have common child(ren)
+            but are not connected.
+        """
+        ## skeleton: a list of edges (undirected)
+        skeleton = self.get_skeleton()
+        ## find immoralities
+        immoral = set()
+        for (nname, node) in self.nodes.iteritems():
+            ## check each pair of parents
+            if len(node.parents) > 1:
+                parents = node.parents.keys()
+                for i1 in xrange(len(parents)-1):
+                    for i2 in xrange(i1+1, len(parents)):
+                        p1, p2 = parents[i1], parents[i2]
+                        ## parents are immoral if they are not connected
+                        if ((p1, p2) not in skeleton and (p2, p1) not in skeleton 
+                            and (p1, p2) not in immoral and (p2, p1) not in immoral):
+                            immoral.add((p1, p2))
+
+        return (skeleton, immoral)
+
+
+
+
+
+
+
---
+++ src/iequiv.py
@@ -0,0 +1,140 @@
+####################################################
+## I-equivalence of Bayesian Networks
+## Lingxue Zhu
+## 2017/02/05
+####################################################
+##
+## Description:
+##
+## Given two Bayesian Networks, the algorithm tests whether
+## they are I-equivalent.
+##
+## Reference: Koller and Friedman (2009), 
+## Probabilistic Graphical Models: Principles and Techniques
+##
+####################################################
+##
+## Input from stdin:
+##
+## 1. First line: N1 M1
+##    where N1 and M1 are the number of nodes and edges, respectively,
+##    in the first Bayesian network.
+## 2. Next M1 lines: A B
+##    Each line denotes a directed edge A --> B in the BN graph.
+## 3. Next line: N2 M2
+##    where N2 and M2 are the number of nodes and edges, respectively,
+##    in the second Bayesian network.
+## 4. Next M2 lines: A B
+##    Each line denotes a directed edge A --> B in the BN graph.
+##
+####################################################
+##
+## Output to stdout:
+##
+## Prints "True" if two graphs are I-equivalent or "False" otherwise.
+##
+####################################################
+##
+## Example:
+##
+## Input from stdin:
+## 3 2
+## A B
+## B C
+## 3 2
+## C B
+## A B
+##
+## Output to stdout:
+## False
+##
+## This compares two BNs:
+## (1) A --> B --> C
+## (2) A --> B <-- C
+## and prints "False" since they are not I-equivalent.
+##
+## See ../tests/iequiv/* for more examples. To run the tests:
+##
+## $ python dsep.py < ../tests/iequiv/test1.in
+## 
+####################################################
+
+
+
+import sys
+from BN import *
+
+
+def check_iequv(bn1, bn2):
+    """
+    Check whether two Bayesian Networks are I-equivalent.
+    Two BN are I-equivalent if and only if they have the same
+    skeletons and immoralities
+
+    Args:
+        bn1: an instance of BN class, the first BN.
+        bn2: an instance of BN class, the second BN.
+
+    Returns:
+        True if bn1 and bn2 are I-equivalent, and False otherwise.
+    """
+    ## skeletons and immoralities
+    (ske1, immor1) = bn1.get_skeleton_immor()
+    (ske2, immor2) = bn2.get_skeleton_immor()
+
+    ## comparison. 
+    if len(ske1) != len(ske2) or len(immor1) != len(immor2):
+        return False
+
+    ## Note that the edges are undirected so we need to check both ordering
+    for (n1, n2) in immor1:
+        if (n1, n2) not in immor2 and (n2, n1) not in immor2:
+            return False
+    for (n1, n2) in ske1:
+        if (n1, n2) not in ske2 and (n2, n1) not in ske2:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    ########################
+    ## Read from stdin
+    ########################
+    ## First BN
+    ## header line: number of nodes and edges for the first BN. 
+    header1 = sys.stdin.readline().rstrip().split(" ")
+    (nnode1, nedge1) = map(int, header1)
+    ## edges
+    edges1 = []
+    for line in xrange(nedge1):
+        edge = sys.stdin.readline().rstrip().split(" ")
+        edges1 += [edge]
+
+    ## Second BN
+    ## header line: number of nodes and edges for the second BN. 
+    header2 = sys.stdin.readline().rstrip().split(" ")
+    (nnode2, nedge2) = map(int, header2)
+    ## edges
+    edges2 = []
+    for line in xrange(nedge2):
+        edge = sys.stdin.readline().rstrip().split(" ")
+        edges2 += [edge]
+
+    ########################
+    ## Build Graph
+    ########################
+    firstBN = BN()
+    for edge in edges1:
+        firstBN.add_edge(edge)
+
+    secondBN = BN()
+    for edge in edges2:
+        secondBN.add_edge(edge)
+
+
+    ########################
+    ## Check I-equivalence
+    ########################
+    print check_iequv (firstBN, secondBN)
+
+
